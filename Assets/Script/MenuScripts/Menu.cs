12cf4126f904f2ea877a8c066df05b8950c4e71d
suan9340 <sue9340@gmail.com>
2021-10-17 23:58:42 +0900

    d

--- Assets/Script/MenuScripts/Menu.cs
+++ Assets/Script/MenuScripts/Menu.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject DonClickChang;  // 클릭 방지용 창
+    [SerializeField] private GameObject OutGameChang;   // 겜 종료 창
+    private bool isOutGame = false;
     // 게임 시작하기 버튼 눌렀을 때 실행되는 함수
     public void OnClickStartGame()
     {
@@ -25,7 +28,33 @@ public class Menu : MonoBehaviour
     // 게임 종료 버튼 눌렀을 때 실행되는 함수
     public void OnClickOutGame()
     {
-        Debug.Log("게임 종료에 성공!!");
+        isOutGame = !isOutGame;
+        if(isOutGame)
+        {
+            OutGameChang.SetActive(true);
+            DonClickChang.SetActive(true);
+        }
+        else
+        {
+            OutGameChang.SetActive(false);
+            DonClickChang.SetActive(false);
+
+        }
+    }
+
+    // 게임 종료 창 끄는 거 눌렀을 때 함수
+    public void OnClickOutGameXXXX()
+    {
+        isOutGame = !isOutGame;
+        OutGameChang.SetActive(false);
+        DonClickChang.SetActive(false);
+
+    }
+
+    public void OnClickOutYesYes()
+    {
+        Debug.Log("게임 종료 성공!!");
         Application.Quit();
     }
+
 }
