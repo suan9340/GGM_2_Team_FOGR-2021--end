d6fb101c4126fbb3a46aac833d50ac805763e238
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2021-12-05 22:29:34 +0900

    오디오 설정 및 게임 다듬기

--- Assets/Script/MainScripts/MenuScripts/SettingManager.cs
+++ Assets/Script/MainScripts/MenuScripts/SettingManager.cs
@@ -1,14 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 public class SettingManager : MonoBehaviour
 {
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider MasterSoundSlider;
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider soundSlider;
-    public void Apply()
+    public void ChangeBGMVolume(float volume)
     {
-
+        if (volume <= -30f)
+        {
+            audioMixer.SetFloat("BGM", -80f);
+        }
+        else
+        {
+            audioMixer.SetFloat("BGM", volume);
+        }
+    }
+    public void ChangeMasterVolume(float volume)
+    {
+        Debug.Log(volume);
+        if (volume <= -30)
+        {
+            audioMixer.SetFloat("Master", -80f);
+        }
+        else
+        {
+            audioMixer.SetFloat("Master", volume);
+        }
+    }
+    public void ChangeSFXVolume(float volume)
+    {
+        if (volume <= -30)
+        {
+            audioMixer.SetFloat("SFX", -80f);
+        }
+        else
+        {
+            audioMixer.SetFloat("SFX", volume);
+        }
     }
     public void Exit()
     {
