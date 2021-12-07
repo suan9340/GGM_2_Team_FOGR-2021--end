4310fdccf7b1285c644e2348257803706d408bdd
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2021-12-07 09:14:44 +0900

    로고 작업

--- Assets/Script/MainScripts/MenuScripts/CursorManager.cs
+++ Assets/Script/MainScripts/MenuScripts/CursorManager.cs
@@ -7,6 +7,6 @@ public class CursorManager : MonoBehaviour
     [SerializeField] Texture2D cursorImg;
     void Start()
     {
-        Cursor.SetCursor(cursorImg, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(cursorImg, new Vector2(cursorImg.width * 0.5f, cursorImg.height * 0.5f), CursorMode.ForceSoftware);
     }
 }
